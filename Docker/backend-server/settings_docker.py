4bc4a7fb5d8e6bf8642cbf8969b6cdfaa716ca63
Itay Zandbank <itay@chelem.co.il>
2019-03-03 15:46:32 +0200

    Move to gunicorn, fix nginx settings, work on Django settings.

--- Docker/backend-server/settings_docker.py
+++ Docker/backend-server/settings_docker.py
@@ -10,6 +10,8 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(
     sys.modules[os.environ['DJANGO_SETTINGS_MODULE']].__file__))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -54,3 +56,115 @@ make_path(IMAGE_SERVER_ADMIN_UPLOAD_DIR)
 IMAGE_SERVER_ZOOMIFY = 'http://%s%s?zoomify=%s/'
 IMAGE_SERVER_PATH = '/iip/iipsrv.fcgi'
 IMAGE_SERVER_URL = 'http://%s%s' % (IMAGE_SERVER_HOST, IMAGE_SERVER_PATH)
+
+PROJECT_LOG_PATH = '/logs'
+
+MEDIA_ROOT = '/django/media'
+make_path(MEDIA_ROOT)
+STATIC_ROOT = '/django/static'
+make_path(STATIC_ROOT)
+
+ANNOTATIONS_URL = 'uploads/annotations/'
+ANNOTATIONS_ROOT = os.path.join(MEDIA_ROOT,
+                                ANNOTATIONS_URL.strip('/'))
+
+make_path(ANNOTATIONS_ROOT)
+
+# Images uploads
+UPLOAD_IMAGES_URL = 'uploads/images/'
+UPLOAD_IMAGES_ROOT = os.path.join(MEDIA_ROOT,
+                                  UPLOAD_IMAGES_URL.strip('/'))
+make_path(UPLOAD_IMAGES_ROOT)
+
+# Image cache
+IMAGE_CACHE_URL = 'uploads/images/tmp/'
+IMAGE_CACHE_ROOT = os.path.join(MEDIA_ROOT,
+                                IMAGE_CACHE_URL.strip('/'))
+
+make_path(IMAGE_CACHE_ROOT)
+
+# Caches
+DJANGO_CACHE_PATH = os.path.join('django', 'cache')
+make_path(DJANGO_CACHE_PATH)
+FILE_BASED_CACHE_BACKEND = 'digipal.middleware.FileBasedCacheArchetype'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    },
+    'django-compressor': {
+        'BACKEND': FILE_BASED_CACHE_BACKEND,
+        'LOCATION': os.path.join(DJANGO_CACHE_PATH, 'django_compressor'),
+        'TIMEOUT': 60 * 60 * 24,
+        'MAX_ENTRIES': 300,
+    },
+    'digipal_faceted_search': {
+        'BACKEND': FILE_BASED_CACHE_BACKEND,
+        'LOCATION': os.path.join(DJANGO_CACHE_PATH, 'faceted_search'),
+        'TIMEOUT': 60 * 60 * 24,
+        # 'TIMEOUT': 1,
+        'MAX_ENTRIES': 300,
+    },
+    'digipal_compute': {
+        'BACKEND': FILE_BASED_CACHE_BACKEND,
+        'LOCATION': os.path.join(DJANGO_CACHE_PATH, 'compute'),
+        'TIMEOUT': 60 * 60 * 24,
+        # 'TIMEOUT': 1,
+        'MAX_ENTRIES': 300,
+    },
+    'digipal_text_patterns': {
+        'BACKEND': FILE_BASED_CACHE_BACKEND,
+        'LOCATION': os.path.join(DJANGO_CACHE_PATH, 'text_patterns'),
+        'TIMEOUT': 60 * 60 * 24,
+        # 'TIMEOUT': 1,
+        'MAX_ENTRIES': 300,
+    }
+}
+
+# Logs
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'digipal_debug': {
+            'format': '[%(asctime)s] %(levelname)s %(message)s (%(module)s)',
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        # see digipal.utils.dplog()
+        'digipal_debug': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'digipal_debug',
+            'filename': os.path.join(PROJECT_LOG_PATH, 'digipal.log'),
+            'backupCount': 10,
+            'maxBytes': 10 * 1024 * 1024,
+        },
+        'digipal_error': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'digipal_debug',
+            'filename': os.path.join(PROJECT_LOG_PATH, 'error.log'),
+            'backupCount': 10,
+            'maxBytes': 10 * 1024 * 1024,
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['digipal_error', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        # see digipal.utils.dplog()
+        'digipal_debugger': {
+            'handlers': ['digipal_debug'],
+            'level': DEBUG,
+            'propagate': False,
+        },
+    },
+}
--- Docker/backend-server/start.sh
+++ Docker/backend-server/start.sh
@@ -3,5 +3,6 @@
 cd /digipal
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+gunicorn --workers 5 --bind 0.0.0.0:8090 build.wsgi:application --log-file /logs/guncorn.log
 
 /usr/sbin/uwsgi --ini /digipal/wsgi.ini
