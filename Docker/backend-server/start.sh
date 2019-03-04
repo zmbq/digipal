ea2dba42ee8b781a3addd9799a66a1caecbe6a19
Itay Zandbank <itay@chelem.co.il>
2019-03-04 10:22:09 +0200

    Wait for postgres to start properly before running backend

--- Docker/backend-server/start.sh
+++ Docker/backend-server/start.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
+until PGPASSWORD="dppsqlpass" psql -h archetype-db -U app_digipal --db digipal -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 10
+done
+
+echo Postgres is ready.
+
 cd /digipal
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
